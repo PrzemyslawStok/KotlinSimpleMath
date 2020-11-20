420f495c16176910bc32a06b4e357469793401b3
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-11-20 16:44:50 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,13 +2,15 @@ fun main(){
     val tablica = arrayOf(1,2,3,4,5,6)
 
     println("Rozmiar tablicy: ${tablica.size}")
+    println("Indeksy tablicy: ${tablica.indices}")
 
     for(i in 0..tablica.size-1){
         println("tablica[$i]=${tablica[i]}")
     }
 
-
-
+    for(i in tablica.indices){
+        println("tablica[$i]=${tablica[i]}")
+    }
 
     var sumaElementow = 0
 
